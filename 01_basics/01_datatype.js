e3120ca90722d1f1b1c5b67cf57a978f0b63e4f5
yadavjyoti1018 <134961628+yadavjyoti1018@users.noreply.github.com>
2023-09-30 05:49:42 +0000

    datatype

---
+++ 01_basics/01_datatype.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all js code as newer version
+
+// alert("hello") this works when we use browser . But now we are using V8 engine so it will show error.
+
+let name = "jyoti"
+let age = 26
+let isLoggedIn = false
+let state = null
+
+//PRIMITIVE DATATYPES
+// number => to power 53
+// bigint
+// string => ""
+// boolean =>true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// OBJECT
+
+console.log(typeof "jyoti");
+console.log(typeof null);
+console.log(typeof undefined);
+
