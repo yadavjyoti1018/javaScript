1144b765742b8d4a628bd35a6d259a0d11dbbcf6
yadavjyoti1018 <134961628+yadavjyoti1018@users.noreply.github.com>
2023-09-30 06:21:57 +0000

    conversionOperation

---
+++ 01_basics/conversionOperation.js
@@ -0,0 +1,23 @@
+let score ="77abc"  // null // undefined // boolean
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33" =>33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = 0
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+let a = 33
+let string = String(a)
+console.log(string)
+console.log(typeof string);
+
